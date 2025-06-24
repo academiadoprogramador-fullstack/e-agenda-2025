24ca7a938ec5aa42b9e3bf2a7eecb1468e5977d2
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-06-24 13:06:25 -0300

    Implementado action filter de validação

---
+++ eAgenda.WebApp/ActionFilters/ValidateModelAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAgenda.WebApp.ActionFilters;
+
+public class ValidarModeloAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        var modelState = filterContext.ModelState;
+
+        if (!modelState.IsValid)
+        {
+            var controller = (Controller)filterContext.Controller;
+
+            var viewModel = filterContext.ActionArguments.First().Value;
+
+            filterContext.Result = controller.View(viewModel);
+        }
+    }
+}

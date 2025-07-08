facb453b8e77af569d20129239c7b9976ad10bcd
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-07-08 14:47:08 -0300

    Implementado projeto de Banco de Dados SQL

--- eAgenda.WebApp/Program.cs
+++ eAgenda.WebApp/Program.cs
@@ -29,7 +29,7 @@ public class Program
 
         builder.Services.AddScoped<IDbConnection>(provider =>
         {
-            var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=eAgendaDb;Integrated Security=True";
+            var connectionString = builder.Configuration["SQL_CONNECTION_STRING"];
 
             return new SqlConnection(connectionString);
         });
